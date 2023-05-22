58843ba2ecc2201fda11bf20674fa2c32865fa21
Thibault NGUYEN <thibault.nguyen@ecam-rennes.com>
2023-05-22 21:22:47 +0000

    public class MapGenerator attributes

---
+++ MapGenerator.java
@@ -0,0 +1,16 @@
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+public class MapGenerator
+{
+    public int map[][];
+    public int brickWidth;
+    public int brickHeight;
+
+    public MapGenerator (int row, int col)
+    {
+
+
+    }
+}
