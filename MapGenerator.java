105710e64b6881186700f2c218029536cf1a78c1
Thibault NGUYEN <thibault.nguyen@ecam-rennes.com>
2023-05-23 17:31:22 +0000

    MapGenerator method

--- MapGenerator.java
+++ MapGenerator.java
@@ -10,7 +10,16 @@ public class MapGenerator
 
     public MapGenerator (int row, int col)
     {
+        map = new int[row][col];
+        for(int i = 0; i < map.length; i++)
+        {
+            for(int j = 0; j < map[0].length; j++)
+            {
+                map[i][j] = 1;
+            }
+        }
 
-
+        brickWidth = 540/col;
+        brickHeight = 150/row;
     }
 }
